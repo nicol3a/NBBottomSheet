717f91e231466a46a58486037a81dcdb46ead2c9
Nicolas Bichon <nbichon@nuglif.com>
2018-11-15 20:08:23 -0500

    Update Example UI

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -9,29 +9,88 @@
 import UIKit
 import NBBottomSheet
 
+enum BottomSheetType: Int, CaseIterable {
+    case alert
+    case datePicker
+    case tableView
+
+    var title: String {
+        switch self {
+        case .alert:
+            return "Alert example"
+        case .datePicker:
+            return "Date picker example"
+        case .tableView:
+            return "Table view example"
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     // MARK: - Properties
 
+    @IBOutlet weak var tableView: UITableView!
+
+    private let types: [BottomSheetType] = BottomSheetType.allCases
     private let bottomSheetController = NBBottomSheetController()
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupTableView()
+    }
+
+    // MARK: - Setup
+
+    private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "BottomSheetTypeCell")
+    }
 
     // MARK: - Actions
 
-    @IBAction func alertButtonTapped(sender: AnyObject?) {
-        let viewController = AlertViewController()
-        bottomSheetController.present(viewController, on: self)
-    }
+    private func presentBottomSheet(with type: BottomSheetType) {
+        var viewController: UIViewController!
 
-    @IBAction func tableViewButtonTapped(sender: AnyObject?) {
-        let viewController = TableViewController()
-        bottomSheetController.present(viewController, on: self)
-    }
+        switch type {
+        case .alert:
+            viewController = AlertViewController()
+        case .datePicker:
+            viewController = DatePickerViewController()
+        case .tableView:
+            viewController = TableViewController()
+        }
 
-    @IBAction func datePickerButtonTapped(sender: AnyObject?) {
-        let viewController = DatePickerViewController()
         bottomSheetController.present(viewController, on: self)
     }
 
 }
 
+// MARK: - UITableViewDataSource
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return types.count
+    }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BottomSheetTypeCell", for: indexPath)
+
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = BottomSheetType(rawValue: indexPath.row)?.title
+
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+
+        if let type = BottomSheetType(rawValue: indexPath.row) {
+            presentBottomSheet(with: type)
+        }
+    }
+}
